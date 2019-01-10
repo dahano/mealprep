ae5fd712b4d43e9173bfd916c0c437c5ff64fab5
Ofir Dahan <ofirdahan112@gmail.com>
2019-01-10 12:19:08 -0800

    Dashboard Rows layout version marker

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,9 @@
 class SearchController < ApplicationController
   def search
-    @macro = FoodItem.where(name: "#{params[:name]}").first
-    
+    puts "SEARCH PARAMS: #{params[:search]}"
+    @macro = FoodItem.find_food_item_name(params[:search])
+    @macro
+    render 'dashboard/index'
   end
 
   def create_item
