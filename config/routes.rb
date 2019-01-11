a16256d92b94df1b61191519791b674e149c8a19
Ofir Dahan <ofirdahan112@gmail.com>
2019-01-11 15:03:33 -0800

    added create food item modal

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'application#index'
   get 'search', to: 'search#search'
   get 'dashboard', to: 'dashboard#index', as: :dashboard
-  get 'create_item' => 'search#create_item'
+  get 'create_item' => 'search#create_item', as: :create_item
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
