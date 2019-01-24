06c16fff72aabc88d3890b63c8937a86df468385
Ofir Dahan <ofirdahan112@gmail.com>
2019-01-23 21:45:23 -0800

    added dynamic meals to cards

--- app/assets/javascripts/add_to_list.js
+++ app/assets/javascripts/add_to_list.js
@@ -1,17 +1,22 @@
 addFoodItemsToList = function() {
   // Get position
+  var columnName = document.getElementById("meow").parentNode.children[0].innerText
   var mealList = document.getElementsByClassName('meal-card-list')[0]
   var mealListName = document.getElementById("meal-card-name").value
   var mealListAmount = document.getElementById("meal-card-amount").value
 
   var createLi = document.createElement("LI");
+  createLi.className = "list-inline-item list-group-item"
   var concatName = document.createTextNode(mealListName + mealListAmount)
 
-  createLi.className = "list-inline-item list-group-item";
-
+  console.log("Col Name: " + columnName);
   console.log("listname: " + mealListName)
   console.log("ListAmount: " + mealListAmount)
 
   createLi.appendChild(concatName);
   mealList.appendChild(createLi)
+}
+
+function newMealCard() {
+  alert("Meow!")
 }
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class DashboardController < ApplicationController
   def index
   end
+
+  def new_card
+    @carbs = params[:carbs]
+    respond_to do |format|
+      format.js
+    end
+  end
 end
